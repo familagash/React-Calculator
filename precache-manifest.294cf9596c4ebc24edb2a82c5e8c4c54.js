ffc0b057861d7e356347d7e9fc2b44d8b20119c9
familagash <familagash@gmail.com>
2019-10-01 01:05:09 +0400

    Updates

---
+++ precache-manifest.294cf9596c4ebc24edb2a82c5e8c4c54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d73c1e667bca2b30da44a00464db493c",
+    "url": "/React-Calculator/index.html"
+  },
+  {
+    "revision": "9577dfa253e59511df03",
+    "url": "/React-Calculator/static/css/main.74607a78.chunk.css"
+  },
+  {
+    "revision": "b0dbe2ac76538f051d38",
+    "url": "/React-Calculator/static/js/2.dcee9da4.chunk.js"
+  },
+  {
+    "revision": "9577dfa253e59511df03",
+    "url": "/React-Calculator/static/js/main.92edaac4.chunk.js"
+  },
+  {
+    "revision": "0c04a1e12521944221a3",
+    "url": "/React-Calculator/static/js/runtime-main.6a6326c1.js"
+  }
+]);
